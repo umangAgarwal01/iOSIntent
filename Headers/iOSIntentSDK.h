da1d5f86a05a857052a94d3b231ef37a2ac7396b
Trupay <umang@trupay.in>
2017-08-28 16:11:12 +0530

    first commit

---
+++ Headers/iOSIntentSDK.h
@@ -0,0 +1,19 @@
+//
+//  iOSIntentSDK.h
+//  iOSIntentSDK
+//
+//  Created by Trupay on 22/04/17.
+//  Copyright © 2017 Trupay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSIntentSDK.
+FOUNDATION_EXPORT double iOSIntentSDKVersionNumber;
+
+//! Project version string for iOSIntentSDK.
+FOUNDATION_EXPORT const unsigned char iOSIntentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSIntentSDK/PublicHeader.h>
+
+
